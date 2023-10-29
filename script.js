134cceb17c0326bfc6874a1a5a254fb5f8c1bda5
Utero93 <nirvanautero93@gmail.com>
2023-10-29 13:32:53 -0400

    Setting up the beginning dynamic functions for the html page

--- script.js
+++ script.js
@@ -1,149 +1,53 @@
-const questions = [
-    {
-        question: "Inside which HTML element do we put the JavaScript?", 
-        answers: [
-            {text: "<scripting>", correct: false},
-            {text: "<javascript>", correct: false},
-            {text: "<js>", correct: false},
-            {text: "<script>", correct: true},
-        ]
-    },
-    {
-        question: "What is the correct syntax for referring to an external script called 'xxx.js'?", 
-        answers: [
-            {text: "<script name ='xxx.js'>", correct: false},
-            {text: "<script href='xxx.js'>", correct: false},
-            {text: "<script src='xxx.js'>", correct: true},
-            {text: "<script rel='xxx.js'>", correct: false},
-        ]
-    },
-    {
-        question: "How do we write hello in an alert box?", 
-        answers: [
-            {text: "msg('Hello World');", correct: false},
-            {text: "alertBox('Hello World');", correct: false},
-            {text: "alert('Hello World');", correct: true},
-            {text: "msgBox('Hello World');", correct: true},
-        ]
-    },
-    {
-        question: "How do you create a function in Javascript?", 
-        answers: [
-            {text: "function:myFunction()", correct: false},
-            {text: "function myFunction()", correct: true},
-            {text: "function = myFunction()", correct: false},
-            {text: "function = !myFunction()", correct: true},
-        ]
-    },
-    {
-        question: "How do you call a function nameed myFunction?", 
-        answers: [
-            {text: "call myFunction()", correct: false},
-            {text: "call function myFunction()", correct: false},
-            {text: "myFunction()", correct: true},
-            {text: "set myFunction()", correct: false},
-        ]
-    }, 
-    {
-        question: "How to write an IF statement in Javascript?", 
-        answers: [
-            {text: "if i = 5", correct: false},
-            {text: "if (i == 5)", correct: true},
-            {text: "if i == 5 then", correct: false},
-            {text: "if i = 5 then", correct: false},
-        ]
-    }, 
-    {
-        question: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?", 
-        answers: [
-            {text: "if i =! 5 then", correct: false},
-            {text: "if (i<>5)", correct: false},
-            {text: "if (i != 5)", correct: true},
-            {text: "if i <> 5", correct: false},
-        ]
-    }, 
-    {
-        question: "How does a FOR loop start?", 
-        answers: [
-            {text: "for (i <= 5; i++)", correct: false},
-            {text: "for i = 1 to 5", correct: false},
-            {text: "for (i = 0; i <= 5; i++)", correct: true},
-            {text: "for (i=0; i<=5)", correct: false},
-        ]
-    }, 
-    {
-        question: "How can you add a comment in a Javascript?", 
-        answers: [
-            {text: "<-- -->", correct: false},
-            {text: "//", correct: true},
-            {text: "[** **]", correct: false},
-            {text: "<.. ..>", correct: false},
-        ]
-    }, 
-    {
-        question: "How to insert a comment that has more than one line?", 
-        answers: [
-            {text: "/*This comment has more than one line*/", correct: true},
-            {text: "<!--This comment has more than one line-->", correct: false},
-            {text: "<!--This comment has more than one line--!>", correct: false},
-            {text: "//This comment has more than one line//", correct: false},
-        ]
-    }
-];
+// User Story
 
-const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("nxt-btn");
-
-let currentQuestionIndex = 0;
-let score = 0;
-
-function startQuiz(){
-    currentQuestionIndex = 0;
-    score = 0;
-    nextButton.textContent = "Next";
-    showQuestion();
-}
-
-function showQuestion(){
-    resetState();
-    let currentQuestion = questions[currentQuestionIndex];
-    let questionNo = currentQuestionIndex + 1;
-    questionElement.textContent = questionNo + ". " + currentQuestion.
-    question;
-
-    currentQuestion.answers.forEach(answer => {
-        const button = document.createElement("button");
-        button.textContent = answer.text;
-        button.classList.add("btn");
-        answerButtons.appendChild(button);
-        if(answer.correct){
-            button.dataset = answer.correct;
-        }
-        button.addEventListener("click", selectAnswer);
-    });
-
-}
+/* As a student
+I want to take a timed quiz on the basics of Javascript that stores a high score 
+So that i can accurately see my progress compared to the other students in the class
+*/
 
 
-function resetState(){
-    nextButton.style.display = "none";
-    while(answerButtons.firstChild)
-        answerButtons.removeChild(answerButtons.firstChild);
-}
+// Variables with a global scope
+let timeInterval;
+let score; 
+let scoresEl = document.getElementById("scores");
+let timerEl = document.getElementById("timer"); 
+
+// Variables for the quiz home page
+let homeEl = document.getElementById("home"); 
+let startEl = document.getElementById("start"); 
+
+// Variables for the quiz questions/answers
+let quizEl = document.getElementById("quiz");
+let questionEl = document.getElementById("questions");
+let acCheckEl = document.getElementById("answer-check");
+let questionI = 0;
+
+// Variables for the end of quiz section 
+let endEl = document.getElementById("end");
+let submitEl = document.getElementById("submit");
+let inputEl = document.getElementById("input");
+let finalScoreEl = document.getElementById("finalscore");
+
+// Variables for the scoreboard section 
+
+let scoreSectionEl = document.getElementById("score");
+let scoreboardEl = document.getElementById("scoreboard");
+let retryEl = document.getElementById("retry");
+let clearEl = document.getElementById("clear");
 
 
-function selectAnswer(e){
-    const selectedBtn = e.target;
-    const isCorrect = selectedBtn.dataset.correct === "true";
-    if(isCorrect){
-        selectedBtn.classList.add("correct");
-    }else{
-        selectedBtn.classList.add("incorrect");
-    }
-}
+// Dynamic functions for the HTML file
 
-startQuiz();
+startEl.addEventListener("click", function(){
+
+// when clicked the .remove method is called to remove the variable homeEl with the html id "#home"
+// Upon removal of homeEl, the quizEl is then displaying the html section element with the id "#quiz" 
+homeEl.remove();
+quizEl.style.display = "block";
 
 
 
+
+});
+
+// 
